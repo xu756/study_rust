e5ff1726b31117f2aaf2226fee8ef4f6629e2536
xu756 <756334744@qq.com>
2023-11-24 01:52:26 +0800

    router

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use crate::router::MyLogin;
 
 // main.rs
 mod router;
 
-fn main() {
-    router::init_router();
-    let login = router::Login::create("username".to_string(), "password".to_string());
-    println!("username: {}, password: {}", login.username, login.password);
+#[tokio::main]
+async fn main() {
+    router::init_router().await;
 }
--- src/router/router.rs
+++ src/router/router.rs
@@ -1,24 +1,54 @@
 use axum::{
     routing::get,
     Router,
+    Json,
 };
-use std::net::SocketAddr;
+use serde::{Serialize};
 
+// 结构体定义
+#[derive(Serialize)]
+struct User {
+    name: String,
+    age: u8,
+}
 
-pub fn init_router() {
-    let app = route("/", post(handler)); // http://127.0.0.1:3000
+// 处理器
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&addr)
+// 初始化路由
+pub async fn init_router() {
+    let app = Router::new().route("/", get(handler));
+
+    // 使用 hyper 在 localhost:8989 上运行服务器
+    axum::Server::bind(&"0.0.0.0:8989".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
 
-
-// 处理器
-async fn handler() -> &'static str {
-    "Hello, World!"
+#[derive(Serialize)]
+pub struct Response<T> {
+    err_code: u32,
+    err_msg: String,
+    data: T,
 }
+
+pub fn success<T>(data: T) -> Json<Response<T>> {
+    let res = Response {
+        err_code: 0,
+        err_msg: "".to_string(),
+        data,
+    };
+    Json(res)
+}
+
+
+async fn handler() -> Json<Response<User>> {
+    let user = User {
+        name: "Jack".to_string(),
+        age: 18,
+    };
+    success(user)
+}
+
+
