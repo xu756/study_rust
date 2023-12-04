e247b36ac0b031d832c111b2304e149a3b63a82a
xu756 <756334744@qq.com>
2023-12-04 10:56:41 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,9 @@
 mod router;
-
+mod study;
 use router::init_router;
-use std::fs;
-use std::fs::File;
+
 use std::io::{Read, Write};
-use sqlx::postgres::PgPoolOptions;
 #[tokio::main]
 async fn main() {
     init_router().await;
-}
-
-#[derive(Debug)]
-struct User {
-    name: String,
-    age: u8,
-}
-
-impl User {
-    fn new(name: String, age: u8) -> Self {
-        Self { name, age }
-    }
-    fn get_name(&self) -> &String {
-        &self.name
-    }
-    fn set_name(&mut self, name: String) {
-        self.name = name
-    }
-}
-#[test]
-fn struct_test() {
-    let mut user = User::new("Jack".to_string(), 18);
-    println!("age: {}", user.age);
-    println!("name: {}", user.get_name());
-    user.set_name("Tom".to_string());
-    println!("new name: {}", user.get_name());
-}
-
-#[test]
-fn enum_test() {
-    // enum Color {
-    //     Red,
-    //     Green,
-    //     Blue,
-    //     New(u8, u8, u8),
-    // }
-    // let color = Color::New(255, 0, 0);
-    // match color {
-    //     Color::Red => println!("red"),
-    //     Color::Green => println!("green"),
-    //     Color::Blue => println!("blue"),
-    //     Color::New(r, g, b) => println!("rgb: {}, {}, {}", r, g, b),
-    // }
-}
-
-#[test]
-fn file_open_test() {
-    match File::open("hello.txt") {
-        Ok(mut file) => {
-            // 按行读取
-            let mut s = String::new();
-            file.read_to_string(&mut s).unwrap();
-            println!("{}", s);
-        }
-        Err(err) => {
-            println!("{:?}", err)
-        }
-    }
-}
-
-#[test]
-fn file_create_test() {
-    let f = fs::File::create("hello.txt");
-    match f {
-        Ok(mut file) => {
-            file.write(b"hello world").unwrap();
-        }
-        Err(err) => {
-            println!("{:?}", err)
-        }
-    }
-}
-
-#[test]
-fn file_io_test() {
-    // let args = std::env::args();
-    // for arg in args {
-    //     println!("{}", arg);
-    // }
 }
