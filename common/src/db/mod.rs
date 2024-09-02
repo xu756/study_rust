a2c2d387ca6200ee6bdc9d5e08c511c1e0e29bbb
xu756 <756334744@qq.com>
2024-09-02 10:48:25 +0800

    数据库连接

--- common/src/db/mod.rs
+++ common/src/db/mod.rs
@@ -1,14 +1,55 @@
-use crate::error::CodeError;
+mod user;
 
-pub struct DbClient {}
+use crate::error::{db_error, CodeError};
+use sea_orm::{ConnectOptions, Database, DatabaseConnection};
+use std::time::Duration;
+
+pub struct DbClient {
+    pub client: DatabaseConnection,
+}
 
 
 impl DbClient {
+    /**
+     * 连接数据库
+     */
     pub async fn connect() -> Result<DbClient, CodeError> {
-        // 连接数据库
-        // 如果连接失败，返回错误
-        let db = DbClient {};
-        Ok(db)
+        let mut opt = ConnectOptions::new(config::CFG.database.link.clone());
+        opt.max_connections(100)
+            .min_connections(5)
+            .connect_timeout(Duration::from_secs(8)) // 连接超时时间
+            .acquire_timeout(Duration::from_secs(8))    // 获取连接超时时间
+            .idle_timeout(Duration::from_secs(8))   // 空闲连接超时时间
+            .max_lifetime(Duration::from_secs(8))   // 连接最大生命周期
+            .sqlx_logging(true) // 开启日志
+            .sqlx_logging_level(log::LevelFilter::Info);  // 日志级别
+        // .set_schema_search_path("my_schema"); // 设置schema
+
+        let db = Database::connect(config::CFG.database.link.clone()).await;
+        match db {
+            Ok(c) => {
+                Ok(DbClient {
+                    client: c
+                })
+            }
+            Err(e) => {
+                Err(db_error(&format!("connect error: {:?}", e)))
+            }
+        }
+    }
+
+    /**
+     * 关闭数据库连接
+     */
+    pub async fn close(self) -> Result<bool, CodeError> {
+        match self.client.close().await {
+            Ok(_) => {
+                Ok(true)
+            }
+            Err(e) => {
+                Err(db_error(&format!("close error: {:?}", e)))
+            }
+        }
     }
 }
 
@@ -22,7 +63,16 @@ mod tests {
         let db = DbClient::connect().await;
         match db {
             Ok(_) => {
-                println!("connect success")
+                println!("connect success");
+                let close = db.unwrap().close().await;
+                match close {
+                    Ok(_) => {
+                        println!("close success")
+                    }
+                    Err(e) => {
+                        println!("{:?}", e.msg);
+                    }
+                }
             }
             Err(e) => {
                 println!("{:?}", e.msg);
---
+++ common/src/db/user.rs
@@ -0,0 +1,7 @@
+use crate::db::DbClient;
+
+impl DbClient {
+    pub fn add_user(&self) {
+        println!("add user");
+    }
+}
